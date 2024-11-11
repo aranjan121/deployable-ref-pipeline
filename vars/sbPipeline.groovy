95ee9a9a7290a84b9168199a2f58c8cc9898b98c
aranjan121 <ashish.ranjan121@gmail.com>
2024-11-11 14:02:35 +0530

    pipeline code

---
+++ vars/sbPipeline.groovy
@@ -0,0 +1,34 @@
+def call(){
+pipeline {
+    agent any
+    tools {
+ 	 	jdk 'JAVA_HOME'
+  		maven 'MAVEN_HOME'
+	}
+	parameters {
+    choice(
+      description: 'Run flyway database migration using latest master branch from prices in what environment?',
+      name: 'environment',
+      choices: ['TEST','PRE', 'PRO']
+    )
+  }
+
+    stages {
+        stage('Build') {
+            steps {
+			 echo 'Building the aplication...'
+			 bat 'mvn clean install'
+               
+            }
+			
+        }
+		stage('Deploy to Tomcate server') {
+            steps {
+                echo 'Deploy to Tomcate server.....'
+				deploy adapters: [tomcat9(credentialsId: 'TOMCAT', path: '', url: 'http://localhost:8080/')], contextPath: null, war: '**/*.war'
+            }
+        }
+    }
+	
+}
+}
